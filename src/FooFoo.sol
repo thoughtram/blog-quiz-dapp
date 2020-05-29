74953895270095da790eee0a7e6e42ef1b28e037
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2020-05-29 16:29:41 +0200

    First bits

--- src/FooFoo.sol
+++ src/FooFoo.sol
@@ -1,4 +1,118 @@
-pragma solidity ^0.5.15;
+pragma solidity ^0.6.7;
+
+import "ds-test/test.sol";
+
+interface TimeSource {
+    function get_now() external returns (uint);
+}
+
+contract TimeMachine is TimeSource{
+  function get_now() public override returns (uint) {
+    return now;
+  }
+}
+
+enum GameState {
+  Started,
+  RevealPeriod,
+  Revealed,
+  Scammed
+}
 
 contract FooFoo {
+  // For debugging
+  event log_bytes32 (bytes32);
+  event log_named_uint (bytes32 key, uint val);
+
+  bool _revealed;
+  TimeSource private _time_machine;
+
+  // block time from when reveal epoch *should* start
+  uint private _reveal_epoch;
+
+  // block time from when people can call us scammers if we have *NOT* revealed by then.
+  // Everyone will be able to call `get_my_money_back` at that point.
+  uint private _scam_epoch;
+
+  // keccak256(guess + salt) have to result in the following hash to win the quiz
+  bytes32 _winning_hash;
+
+  // The salt will only be known once we reveal, making it impossible for participants to
+  // check if they got it right *before* we reveal the answer
+  string _salt;
+
+  string private _tmp_answer;
+
+  constructor(TimeSource machine,
+              uint reveal_epoch,
+              uint scam_epoch,
+              bytes32 winning_hash
+              ) public payable {
+
+    _revealed = false;
+    _time_machine = machine;
+    _reveal_epoch = reveal_epoch;
+    _scam_epoch = scam_epoch;
+    _winning_hash = winning_hash;
+  }
+
+
+  function get_state() public returns (GameState) {
+    if (_revealed) {
+      return GameState.Revealed;
+    }
+    if (_time_machine.get_now() < _reveal_epoch) {
+      return GameState.Started;
+    } else if (_time_machine.get_now() < _scam_epoch) {
+      return GameState.RevealPeriod;
+    } else if (_time_machine.get_now() >= _scam_epoch) {
+      return GameState.Scammed;
+    }
+  }
+
+
+  // TODO: Everything :)
+  function make_guess(string memory guess) public {
+    _tmp_answer = guess;
+  }
+
+  // TODO: This whole thing needs to become multi player
+  function claim_win() public {
+    GameState state = get_state();
+    if (state == GameState.Started || state == GameState.RevealPeriod) {
+      revert("Be patient! The game is still running.");
+    } else if (state == GameState.Revealed) {
+      bytes32 final_hash = keccak256(abi.encodePacked(_tmp_answer, _salt));
+      emit log_bytes32(final_hash);
+      if (final_hash == _winning_hash) {
+        // TODO: This need to be proportional to how many people got it right
+        // TODO: Also we need to check this can not be claimed multiple times
+        msg.sender.transfer(1 ether);
+      }
+      else {
+        revert("You lost");
+      }
+    } else if (state == GameState.Scammed) {
+      // TODO: Need to proportional pay back fund
+      revert("Not implemented");
+    }
+  }
+
+  function reveal_answer(string memory salt) public {
+    GameState state = get_state();
+    if (state == GameState.Revealed) {
+      revert("Already revealed. Can not reveal again!");
+    } else if (state == GameState.Started) {
+      revert("Can not reveal before reveal_epoch started");
+    } else if (state == GameState.Scammed) {
+      revert("Too late scammer!");
+    } else if (state == GameState.RevealPeriod) {
+      _revealed = true;
+      _salt = salt;
+    } else {
+      revert("Invariant");
+    }
+
+  }
+
 }
--- src/FooFoo.t.sol
+++ src/FooFoo.t.sol
@@ -1,21 +1,103 @@
-pragma solidity ^0.5.15;
+pragma solidity ^0.6.7;
 
 import "ds-test/test.sol";
 
 import "./FooFoo.sol";
 
+contract MockTimeMachine is TimeSource{
+
+  uint private _mocked_now;
+
+  constructor(uint mocked) public payable {
+    _mocked_now = mocked;
+  }
+
+  function get_now() public override returns (uint) {
+    return _mocked_now;
+  }
+
+  function set_now(uint mocked_now) public returns (uint) {
+    return _mocked_now = mocked_now;
+  }
+}
+
 contract FooFooTest is DSTest {
-    FooFoo foo;
+    FooFoo egg;
+    MockTimeMachine _time_machine;
+
+    string SALT = "o";
+    bytes32 WINNING_HASH = 0xf539ba76cb28bd8b154e5e1e046c11cc09c5a4831299ea08e5a04ce250df879f;
 
     function setUp() public {
-        foo = new FooFoo();
+        _time_machine = new MockTimeMachine(1000);
+        egg = (new FooFoo){value: 1 ether}(_time_machine, 2000, 3000, WINNING_HASH);
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
+    function test_initial_state() public {
+        assertEq(address(egg).balance, 1 ether);
+        assertTrue(egg.get_state() == GameState.Started);
     }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function test_reveal_before_reveal_epoch_started() public {
+        assertTrue(egg.get_state() == GameState.Started);
+
+        try egg.reveal_answer(SALT) {
+            fail();
+        } catch Error(string memory reason) {
+            assertEq(reason, "Can not reveal before reveal_epoch started");
+            assertTrue(egg.get_state() == GameState.Started);
+        }
     }
+
+    function test_reveal_after_scam_epoch_started() public {
+        assertTrue(egg.get_state() == GameState.Started);
+        _time_machine.set_now(3000);
+        try egg.reveal_answer(SALT) {
+            fail();
+        } catch Error(string memory reason) {
+            assertEq(reason, "Too late scammer!");
+        }
+    }
+
+    function test_reveal_after_reveal_epoch_started() public {
+        assertTrue(egg.get_state() == GameState.Started);
+        _time_machine.set_now(2000);
+        egg.reveal_answer(SALT);
+        assertTrue(egg.get_state() == GameState.Revealed);
+    }
+
+    function test_reveal_when_already_revealed() public {
+        test_reveal_after_reveal_epoch_started();
+        try egg.reveal_answer(SALT) {
+            fail();
+        } catch Error(string memory reason) {
+            assertEq(reason, "Already revealed. Can not reveal again!");
+        }
+    }
+
+    function test_claim_while_game_is_on() public {
+        egg.make_guess("foo");
+        try egg.claim_win() {
+            fail();
+        } catch Error(string memory reason) {
+            assertEq(reason, "Be patient! The game is still running.");
+        }
+    }
+
+    function test_claim_win_after_revealed() public {
+        uint256 old_balance = address(this).balance;
+        egg.make_guess("foo");
+        _time_machine.set_now(2000);
+        egg.reveal_answer(SALT);
+        egg.claim_win();
+        assertTrue(address(this).balance > old_balance);
+    }
+
+    // function test_guess() public {
+    //     egg.make_guess("foo");
+    //     egg.claim_win();
+    // }
+
+    receive() external payable { }
+
 }
